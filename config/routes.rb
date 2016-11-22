786ff542e3374e4d476091fbf4d280c1bea5002b
Matthew Townsen <matthewtownsen@gmail.com>
2016-11-21 20:05:08 -0700

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     resources :webhooks do
     end
 
-    post 'open' => 'webhooks#webhook'
-    post 'click' => 'webhooks#webhook'
+    get 'open' => 'webhooks#webhook'
+    get 'click' => 'webhooks#webhook'
 end
